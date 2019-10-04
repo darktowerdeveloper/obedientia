98dbd2232a4f1676ad8816e614398fa5c4ff15ab
Douglas Oliveira <perfildoug@gmail.com>
2019-10-03 23:19:53 -0300

    Form Buy - Part 2

--- js/custom.js
+++ js/custom.js
@@ -2,48 +2,38 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    $('#btnBuyEn').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        // var attr = $(this).attr('data-margin');
+        // var margin = 70;
+        // if (typeof attr !== typeof undefined && attr !== false) {
+        //     // Element has this attribute
+        //     margin = $(this).attr('data-margin');
+        // }
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                // scrollTop: target.offset().top - margin
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
     // pt-br
     // $("#formTel").mask("(999) 9999-9999");
     // world => a quantidade de caracteres aqui é referente ao maxlength permitido
     $("#formZipcode").mask("99999");
 
-    $('#formValidate').validate({
+    $('#formBuy').validate({
 
         /* submit via ajax */
-        // submitHandler: function(form) {				
-        //     var sLoader = $('#submit-loader');			
-
-        //     $.ajax({   	
-        //         type: "POST",
-        //         url: "inc/sendEmail.php",
-        //         data: $(form).serialize(),
-
-        //         beforeSend: function() { 
-        //             sLoader.fadeIn(); 
-        //         },
-        //         success: function(msg) {
-        //         // Message was sent
-        //         if (msg == 'OK') {
-        //             sLoader.fadeOut(); 
-        //             $('#message-warning').hide();
-        //             $('#contactForm').fadeOut();
-        //             $('#message-success').fadeIn();   
-        //         }
-        //         // There was an error
-        //         else {
-        //             sLoader.fadeOut(); 
-        //             $('#message-warning').html(msg);
-        //             $('#message-warning').fadeIn();
-        //         }
-        //         },
-        //         error: function() {
-        //             sLoader.fadeOut(); 
-        //             $('#message-warning').html("Something went wrong. Please try again.");
-        //             $('#message-warning').fadeIn();
-        //         }
-        //     });    		
-        // },
-
+        submitHandler: function(form) {
+            $.ajax({   	
+                type: "POST",
+                url: "php/sendEmail.php",
+                data: $(form).serialize(),
+            });    		
+        },
         rules:{
             formName: {
                 required: true,
